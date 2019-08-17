cfb4f16bee654b3564dc42a51c28494925f1db60
Grant Horner <hhorner@s1inc.com>
2019-08-17 17:37:11 -0400

    basic set up

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            var player = new Player {Name = "Pipes", PlayerName = "Gil"};
+            return View(player);
+        }
+    }
+}
---
+++ WebApplication1/Models/Player.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Authorization.Infrastructure;
+
+namespace WebApplication1.Models
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public string PlayerName { get; set; }
+    }
+}
